62a2cd16d582af0b866f3ba47705da46c93ffa6a
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-14 19:10:26 +0000

    update

--- test.py
+++ test.py
@@ -1,20 +1,22 @@
 import requests
 import csv
 
-url = "https://raw.githubusercontent.com/asmaparker/pageturnerr/main/books.csv?"
-i = 0
-with requests.get(url=url, stream=True) as r:
+url = "https://raw.githubusercontent.com/asmaparker/pageturnerr/main/books.csv"
+with requests.get(url=url, stream=False) as r:
     lines = (line.decode('utf-8') for line in r.iter_lines())
     next(lines)
-    for row in csv.reader(lines, delimiter='|'):
-        print(row)
-        print("ISBN:", row[0])
-        print("ISBN13:", row[1])
-        print("Title:", row[2])
-        print("Synopsis:", row[3])
-        print("Publisher:", row[4])
-        print("Author:", row[5])
-        print("Publication Date:", row[6])
-        print("Language:", row[7])
-        print("Price:", row[8])
-        print("Pages:", row[9])
+
+f = open("books.csv", "r")
+lines = f.readlines()
+for row in csv.reader(lines, delimiter='|'):
+    print(row)
+    print("ISBN:", row[0])
+    print("ISBN13:", row[1])
+    print("Title:", row[2])
+    print("Synopsis:", row[3])
+    print("Publisher:", row[4])
+    print("Author:", row[5])
+    print("Publication Date:", row[6])
+    print("Language:", row[7])
+    print("Price:", row[8])
+    print("Pages:", row[9])
