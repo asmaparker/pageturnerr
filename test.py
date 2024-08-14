0664b1b1b95f49a5cf82e433412d63d908610d2b
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-14 19:17:18 +0000

    update

--- test.py
+++ test.py
@@ -1,19 +1,10 @@
 import requests
 import csv
-
+L = []
 url = "https://raw.githubusercontent.com/asmaparker/pageturnerr/main/books.csv"
 with requests.get(url=url, stream=True, headers={'Cache-Control': 'no-cache'}) as r:
     lines = (line.decode('utf-8') for line in r.iter_lines())
     next(lines)
     for row in csv.reader(lines, delimiter='|'):
-        print(row)
-        print("ISBN:", row[0])
-        print("ISBN13:", row[1])
-        print("Title:", row[2])
-        print("Synopsis:", row[3])
-        print("Publisher:", row[4])
-        print("Author:", row[5])
-        print("Publication Date:", row[6])
-        print("Language:", row[7])
-        print("Price:", row[8])
-        print("Pages:", row[9])
+        L.append(row)
+print(L)
