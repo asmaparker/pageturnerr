b53f6205ef3221bdc19865c94e25dcbdd2767c65
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-14 19:12:31 +0000

    update

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 import csv
 
 url = "https://raw.githubusercontent.com/asmaparker/pageturnerr/main/books.csv"
-with requests.get(url=url, stream=False) as r:
+with requests.get(url=url, stream=True, headers={'Cache-Control': 'no-cache'}) as r:
     lines = (line.decode('utf-8') for line in r.iter_lines())
     next(lines)
     for row in csv.reader(lines, delimiter='|'):
