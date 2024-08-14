edb0dd49a164563fec2b1703dec7a7a7ff12f352
tahayparker <98612931+tahayparker@users.noreply.github.com>
2024-08-14 22:56:22 +0400

    update

--- check_val_isbns.py
+++ check_val_isbns.py
@@ -11,14 +11,23 @@ def is_valid_isbn10(isbn):
         total += 10 * int(isbn[-1])
     return total % 11 == 0
 
-f = open("check.txt")
+def is_valid_isbn13(isbn):
+    isbn = isbn.replace('-', '')
+    isbn = isbn.strip()
+    isbn = isbn.replace("\n", "")
+    if len(isbn) != 13 or not isbn.isdigit():
+        return False
+    total = sum(int(x) * (3 if i % 2 else 1) for i, x in enumerate(isbn[:-1]))
+    return (10 - total % 10) % 10 == int(isbn[-1])
+
+f = open("isbns.txt")
 count = 0
 j = 0
 for isbn in f:
     j += 1
-    check = is_valid_isbn10(isbn)
+    check = is_valid_isbn13(isbn)
     count += 1 if not check else 0
-    print(isbn, check)
-
+    if not check:
+        print(isbn)
 print("Invalid ISBN's", count)
 print("Total Valid Books", j - count)
