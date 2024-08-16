0416d1c0dbd32bcd76872271f51c6464f26e4f4c
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-16 14:49:13 +0000

    comments

--- bookstore.py
+++ bookstore.py
@@ -652,7 +652,7 @@ def buy(isbn):
         else: # If the credit card number is valid
             expiration_date = input("Enter the expiration date (MM/YY): ")
             # Check expiration date
-            if not check_cc_expiry(expiration_date):
+            if not check_cc_expiry(expiration_date): 
                 print("Credit card expired!")
                 continue
             else:
