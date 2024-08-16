1c534c4a99f96889f362c9f7a7ddc441b4526a02
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-16 14:48:39 +0000

    comments

--- bookstore.py
+++ bookstore.py
@@ -628,7 +628,7 @@ def delete_account():  # Delete the user account
                 cdb.commit()
                 db.execute("DELETE FROM auth WHERE username = '{}'".format(login_username)) # Delete the user from the auth table
                 cdb.commit()
-                db.execute("DELETE FROM cart WHERE username = '{}'".format(login_username)) # Delete the user's
+                db.execute("DELETE FROM cart WHERE username = '{}'".format(login_username)) # Delete the user's cart
                 cdb.commit()
                 print("Account deleted successfully!")
                 kill()
