520386df549d76835934af3cf20c4881d15eb5b4
asma <154118368+asmaparker@users.noreply.github.com>
2024-08-16 14:22:51 +0400

    groq

--- bookstore.py
+++ bookstore.py
@@ -488,8 +488,7 @@ def list_info(isbn, isbn13=None, title=None, synopsis=None, publisher=None, auth
 
 def ai_suggestions(title, synopsis, year, author, pages):  # Suggest books similar to the one entered
     client = Groq(api_key="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")
-    completion = client.chat.completions.create(model="llama-3.1-8b-instant", messages=[{"role": "system", "content": "You are a helpful assistant, who informs people about similar books that they would be interested to read"}, {"role": "user", "content": f"The book is {
-                                                title}. The synopsis of the book is {synopsis}. It was released in {year}. The authors of the book is/are {author}, and the book is {pages} pages long. Suggest three books similar to this one. Do not format the output (just give a plaintext response)."}])
+    completion = client.chat.completions.create(model="llama-3.1-8b-instant", messages=[{"role": "system", "content": "You are a helpful assistant, who informs people about similar books that they would be interested to read"}, {"role": "user", "content": f"The book is {title}. The synopsis of the book is {synopsis}. It was released in {year}. The authors of the book is/are {author}, and the book is {pages} pages long. Suggest three books similar to this one. Do not format the output (just give a plaintext response)."}])
     return completion.choices[0].message.content
 
 
