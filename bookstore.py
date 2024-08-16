b56c02466d36b626ff28e6f0a619c34c2a56443d
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-16 14:49:00 +0000

    comments

--- bookstore.py
+++ bookstore.py
@@ -646,7 +646,7 @@ def buy(isbn):
     while True:
         # Prompt for credit card details
         card_number = input("Enter your credit card number: ")
-        if luhn(card_number) == False: # Check the credit card number
+        if luhn(card_number) == False: # Check the credit card number;s
             print("Invalid credit card number!")
             continue
         else:
