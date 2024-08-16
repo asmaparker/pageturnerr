3f80ebec0a97df5f78f5b323e9ac4d61a895280e
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-16 14:41:04 +0000

    comments

--- bookstore.py
+++ bookstore.py
@@ -309,7 +309,7 @@ def search_title(title):  # Search for a book by title
         while True:
             ch = int(input("Enter the number of the book you would like to select: ")) # Input the number of the book to select
             if ch <= 10 and ch >= 1:
-                return rs[ch-1][0] 
+                return rs[ch-1][0] # Return the ISBN of the book
             elif ch == 0:
                 return
             else:
