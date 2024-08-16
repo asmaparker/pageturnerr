f7752673091c34590b05b639ba20f88722f7e18c
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-16 14:42:47 +0000

    comments

--- bookstore.py
+++ bookstore.py
@@ -354,9 +354,9 @@ def search_author(author):  # Search for a book by author
 
         while True:
             try:
-                ch = int(input("Enter the number of the book you would like to select: ")) 
+                ch = int(input("Enter the number of the book you would like to select: ")) # Input the number of the book to select
                 if ch <= 10 and ch >= 1:
-                    return rs[ch-1][0]
+                    return rs[ch-1][0] 
                 if ch == 0:
                     return
             except:
