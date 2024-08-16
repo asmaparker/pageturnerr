43e51845f2a781f0dd2ec7d533aa83f746eb916d
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-16 14:43:34 +0000

    comments

--- bookstore.py
+++ bookstore.py
@@ -397,7 +397,7 @@ def search_yearofpublishing(year):  # Search for a book by year of publishing
         j = 0
         for i in rs: # Iterate through the books found
             j += 1
-            print("{}. {}".format(j, i[1])) 
+            print("{}. {}".format(j, i[1])) # Print the title of the book
 
         while True:
             try:
