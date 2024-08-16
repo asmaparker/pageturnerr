34231f96d8c584d84e2ef03039e94281bf296080
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-16 14:50:32 +0000

    comments

--- bookstore.py
+++ bookstore.py
@@ -704,13 +704,9 @@ def list_bought():  # List the books bought by the user
 
 
 def luhn(ccn):  # Check if the credit card number entered is correct
-    c = new_func(ccn)
+    c = [int(x) for x in str(ccn)[::-2]]
     u2 = [(2*int(y))//10+(2*int(y)) % 10 for y in str(ccn)[-2::-2]] 
     return sum(c+u2) % 10 == 0
-
-def new_func(ccn):
-    c = [int(x) for x in str(ccn)[::-2]]
-    return c
 
 
 def check_cc_expiry(expiry):
