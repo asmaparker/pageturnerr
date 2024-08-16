4eadc4d234ede2b8a4807199609c8ccae2302f92
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-16 14:43:22 +0000

    comments

--- bookstore.py
+++ bookstore.py
@@ -384,7 +384,7 @@ def search_price(maxprice, minprice):  # Search for a book by price
                 if ch == 0:
                     return
             except:
-                print(termcolor.colored("Error! Choose a number from the list.", "red")) 
+                print(termcolor.colored("Error! Choose a number from the list.", "red")) # Print an error message if the number is not in the list
 
 
 def search_yearofpublishing(year):  # Search for a book by year of publishing
