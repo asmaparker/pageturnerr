2a83c93119c45b09b8009227b6e98b6d8a0e53f8
Taha Parker <98612931+tahayparker@users.noreply.github.com>
2024-08-16 14:50:05 +0000

    comments

--- bookstore.py
+++ bookstore.py
@@ -697,7 +697,7 @@ def list_bought():  # List the books bought by the user
     j = 0
     for i in rs: # Iterate through the books bought by the user
         j += 1
-        db.execute("SELECT title FROM inventory WHERE isbn = '{}'".format(i[0])) 
+        db.execute("SELECT title FROM inventory WHERE isbn = '{}'".format(i[0])) # Get the title of the book
         rs2 = db.fetchall()
         print("{}. {}".format(j, rs2[0][0]))
     print()
